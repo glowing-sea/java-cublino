1f0dfe8948c324a445ee2a794c39da5a9cc61f3a
Bob <u7227566@anu.edu.au>
2021-08-24 09:47:35 +1000

    Lmao

--- src/gittest/B.java
+++ src/gittest/B.java
@@ -5,4 +5,8 @@ public class B {
     public String toString() {
         return super.toString();
     }
+
+    public static void main(String[] args) {
+        System.out.println("Lol");
+    }
 }
