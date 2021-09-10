f68fef94bb0d8b7f8f332a15f411bd15cbec8548
Rajin Hossain <u7313467@anu.edu.au>
2021-09-10 01:57:00 +0000

    Task 5: Board Visualiser

--- src/comp1140/ass2/core/Piece.java
+++ src/comp1140/ass2/core/Piece.java
@@ -1,25 +1,110 @@
 package comp1140.ass2.core;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Piece {
-    private int facing;
-    private boolean isPlayer1; // make a method which sets the player, and make it final
-    public Piece(int facing, boolean isPlayer1) {
-        this.facing = facing;
+    private int topNumber; // number on the top of the dice
+    private int forwardNumber; // number on the front of the dice away from you
+    private String encoding = ""; // dice encoding
+    private boolean isPlayer1; // the player's type
+    private Location position; // the piece's position
+
+    public Piece(int topNumber,int forwardNumber, boolean isPlayer1, Location position) {
+        this.topNumber = topNumber;
+        this.forwardNumber = forwardNumber;
         this.isPlayer1 = isPlayer1;
+        this.position = position;
     }
+
+    // An intuitive constructor for generating Piece objects from dice encodings
+    public Piece(String encoding) {
+        this.encoding = encoding;
+
+        char orientation = encoding.charAt(0);
+        char column = encoding.charAt(1);
+        char row = encoding.charAt(2);
+
+        if (Character.isUpperCase(orientation)) { // assign the player's type
+            this.isPlayer1 = true;
+        }
+
+
+        // A translation map from the characters available to an array containing [TOP_FACE, FORWARD_FACE]
+        HashMap<Character, int[]> translationTable = new HashMap<>();
+        translationTable.put('a', new int[] {1,2});
+        translationTable.put('b', new int[] {1,3});
+        translationTable.put('c', new int[] {1,4});
+        translationTable.put('d', new int[] {1,6});
+
+        translationTable.put('e', new int[] {2,1});
+        translationTable.put('f', new int[] {2,3});
+        translationTable.put('g', new int[] {2,4});
+        translationTable.put('h', new int[] {2,5});
+
+        translationTable.put('i', new int[] {3,1});
+        translationTable.put('j', new int[] {3,2});
+        translationTable.put('k', new int[] {3,5});
+        translationTable.put('l', new int[] {3,6});
+
+        translationTable.put('m', new int[] {4,1});
+        translationTable.put('n', new int[] {4,2});
+        translationTable.put('o', new int[] {4,5});
+        translationTable.put('p', new int[] {4,6});
+
+        translationTable.put('q', new int[] {5,2});
+        translationTable.put('r', new int[] {5,3});
+        translationTable.put('s', new int[] {5,4});
+        translationTable.put('t', new int[] {5,6});
+
+        translationTable.put('u', new int[] {6,1});
+        translationTable.put('v', new int[] {6,3});
+        translationTable.put('w', new int[] {6,4});
+        translationTable.put('x', new int[] {6,5});
+
+        this.topNumber = translationTable.get(Character.toLowerCase(orientation))[0]; // assign top facing number
+        this.forwardNumber = translationTable.get(Character.toLowerCase(orientation))[1]; // assign forward facing number
+
+        int columnNum = 0;
+        int rowNum = Character.getNumericValue(row);
+
+        if (column == 'a') {
+            columnNum = 1;
+        } else if (column == 'b') {
+            columnNum = 2;
+        } else if (column == 'c') {
+            columnNum = 3;
+        } else if (column == 'd') {
+            columnNum = 4;
+        } else if (column == 'e') {
+            columnNum = 5;
+        } else if (column == 'f') {
+            columnNum = 6;
+        } else if (column == 'g') {
+            columnNum = 7;
+        }
+
+        this.position = new Location(rowNum,columnNum); // assignment of location based on encoding
+    }
+
+    public boolean isPlayer1() {
+        return isPlayer1;
+    }
+
+    public Location getPosition() {
+        return position;
+    }
+
     public Piece[] getAdjacentPieces(Board board) {
         Piece[] def = new Piece[4];
         return def;
     } // returns the adjacent pieces as a list
-
-    public void setFacing(int facing) {
-        this.facing = facing;
+    public void setTopNumber(int topNumber) {
+        this.topNumber = topNumber;
     } // returns the number that is facing up on the dice
 
-    public int getFacing() {
-        return facing;
+    public int getTopNumber() {
+        return this.topNumber;
     }
     public ArrayList<Move> getLegalMoves(Piece p1, Board b1) {
         ArrayList <Move> legalMoves = new ArrayList<>();
--- src/comp1140/ass2/gui/Viewer.java
+++ src/comp1140/ass2/gui/Viewer.java
@@ -1,13 +1,22 @@
 package comp1140.ass2.gui;
 
+import comp1140.ass2.Cublino;
+import comp1140.ass2.core.Piece;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import javax.swing.text.LabelView;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 /**
@@ -27,6 +36,7 @@ public class Viewer extends Application {
 
     private final Group root = new Group();
     private final Group controls = new Group();
+    private final Pane gamePane = new Pane();
     private TextField textField;
 
     /**
@@ -35,7 +45,49 @@ public class Viewer extends Application {
      * @param placement A valid placement string
      */
     void makePlacement(String placement) {
-        // FIXME Task 4 (CR): implement the simple game state viewer
+
+        String placementEncoding = placement.trim();
+        if (Cublino.isStateValid(placementEncoding)) {
+            String diceEncodings = placementEncoding.substring(1,placementEncoding.length()); // all the dice encodings are assumed to be valid
+            ArrayList<Piece> dices = new ArrayList<>();
+
+            // add all the dice pieces
+            for (int i = 0; i < diceEncodings.length()-2; i+=3) {
+                dices.add(new Piece(diceEncodings.substring(i,i+3)));
+            }
+
+            // generate sprites at the right locations
+            for (Piece dice : dices) {
+                String colour = dice.isPlayer1() ? "w" : "b";
+                String topFacing = String.valueOf(dice.getTopNumber());
+                String diceAssetURI = URI_BASE+colour+"_dice_"+topFacing+".png"; // dice image path based on the dice's properties
+
+                ImageView diceImage = new ImageView();
+                diceImage.setImage(new Image(diceAssetURI));
+                diceImage.relocate(19+60*(dice.getPosition().getY()-1),19+60*(dice.getPosition().getX()-1)); // pixel translation
+
+                gamePane.getChildren().add(diceImage); // add the leaf nodes to the parent node
+                System.out.println("added");
+            }
+
+        } else if (!placement.equals("")) { // displays a warning dialog if placement string is invalid
+            Alert a1 = new Alert(Alert.AlertType.WARNING,
+                    "Please Enter A Valid Placement String.", ButtonType.OK);
+            a1.setResizable(false);
+            a1.show();
+        }
+    }
+
+    /* Renders the base game board */
+    private void renderBoard() {
+        gamePane.setMinSize(450,450);
+        gamePane.setLayoutX(240);
+        gamePane.setLayoutY(100);
+        gamePane.setBackground(new Background(new BackgroundImage(new Image(URI_BASE+"board.png"),
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT)));
     }
 
     /**
@@ -47,28 +99,54 @@ public class Viewer extends Application {
         textField.setPrefWidth(300);
         Button refresh = new Button("Refresh");
         refresh.setOnAction(actionEvent -> {
+                // erase all the current dices on the board before rendering the dices
+                if (gamePane.getChildren().size() > 0) {
+                    gamePane.getChildren().clear();
+                    System.out.println("removed dices");
+                }
+
+                // render the dice placements on the board
                 makePlacement(textField.getText());
                 textField.clear();
         });
         HBox hb = new HBox();
+
         hb.getChildren().addAll(label1, textField, refresh);
         hb.setSpacing(10);
         hb.setLayoutX(230);
         hb.setLayoutY(VIEWER_HEIGHT - 50);
-        controls.getChildren().add(hb);
+
+        controls.getChildren().addAll(hb);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Cublino Viewer");
+        primaryStage.setResizable(false);
         Scene scene = new Scene(root, VIEWER_WIDTH, VIEWER_HEIGHT);
 
-        root.getChildren().add(controls);
-
         makeControls();
+        renderBoard();
+
+        root.getChildren().addAll(gamePane, controls);
 
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        // Refresh Key's Alternative Option: Press Enter
+        scene.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.ENTER) {
+                // erase all the current dices on the board before rendering the dices
+                if (gamePane.getChildren().size() > 0) {
+                    gamePane.getChildren().clear();
+                    System.out.println("removed dices");
+                }
+
+                // render the dice placements on the board
+                makePlacement(textField.getText());
+                textField.clear();
+            }
+        });
     }
 
 }
